e8f1f5cdb42baa80150413bc572c50a91b8b5196
Pedro Teixeira <pedro.teix@ua.pt>
2019-12-18 22:32:53 +0000

    Added Guide 4

---
+++ 2-OpenCV/G04-MathOperations/OpenCV_ex_18.cpp
@@ -0,0 +1,121 @@
+/*
+ * OpenCV_ex_18.c
+ *
+ *
+ * J. Madeira - Dec 2012 + Nov 2017
+ */
+
+
+// Visual Studio ONLY - Allowing for pre-compiled header files
+
+// This has to be the first #include
+
+// Remove it, if you are not using Windows and Visual Studio
+
+// #include "stdafx.h"
+
+
+// #include <iostream>
+
+#include <iostream>
+
+#include "opencv2/core/core.hpp"
+
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include "opencv2/highgui/highgui.hpp"
+
+
+// If you want to "simplify" code writing, you might want to use:
+
+// using namespace cv;
+
+// using namespace std;
+
+
+// AUXILIARY  FUNCTION
+
+void printImageFeatures(const cv::Mat &image)
+{
+	std::cout << std::endl;
+
+	std::cout << "Number of rows : " << image.size().height << std::endl;
+
+	std::cout << "Number of columns : " << image.size().width << std::endl;
+
+	std::cout << "Number of channels : " << image.channels() << std::endl;
+
+	std::cout << "Number of bytes per pixel : " << image.elemSize() << std::endl;
+
+	std::cout << std::endl;
+}
+
+
+// MAIN
+
+int main( int argc, char** argv )
+{
+    if( argc != 2 )
+    {
+		std::cout << "The name of the image file is missing !!" << std::endl;
+
+        return -1;
+    }
+
+	cv::Mat originalImage;
+
+	originalImage = cv::imread( argv[1], cv::IMREAD_UNCHANGED );
+
+	if( originalImage.empty() )
+	{
+		// NOT SUCCESSFUL : the data attribute is empty
+
+		std::cout << "Image file could not be open !!" << std::endl;
+
+	    return -1;
+	}
+
+	if( originalImage.channels() > 1 )
+	{
+		// Convert to a single-channel, intensity image
+		cv::cvtColor( originalImage, originalImage, cv::COLOR_BGR2GRAY, 1 );
+	}
+
+	// Create window
+    cv::namedWindow( "Imagem Original", cv::WINDOW_AUTOSIZE );
+	cv::imshow( "Imagem Original", originalImage );
+
+	// Print some image features
+	std::cout << "ORIGINAL IMAGE" << std::endl;
+    printImageFeatures( originalImage );
+
+	// Negative
+	cv::Mat imgA;
+	cv::threshold(originalImage, imgA, 120, 255, CV_THRESH_BINARY_INV);
+
+	// Dilation
+	int dilation_size = 110;
+    cv::Mat result;
+	cv::Mat kernel = cv::getStructuringElement(0, cv::Size( dilation_size, dilation_size ));
+	cv::dilate(imgA, result, kernel);
+
+    cv::namedWindow( "Dilate ", cv::WINDOW_AUTOSIZE );
+    cv::imshow( "Dilate ", result);
+
+    // Result
+	cv::Mat finalResult;
+	cv::subtract(result, imgA, finalResult);
+
+	cv::namedWindow( "Result ", cv::WINDOW_AUTOSIZE );
+    cv::imshow( "Result ", finalResult);
+
+    // Waiting
+
+    cv::waitKey( 0 );
+
+	// Destroy the windows
+
+	cv::destroyAllWindows();
+
+	return 0;
+}
---
+++ 2-OpenCV/G04-MathOperations/OpenCV_ex_20.cpp
@@ -0,0 +1,119 @@
+/*
+ * OpenCV_ex_16.c
+ *
+ * THE SOBEL OPERATOR
+ *
+ * J. Madeira - Dec 2012 + Nov 2017
+ */
+
+
+// Visual Studio ONLY - Allowing for pre-compiled header files
+
+// This has to be the first #include
+
+// Remove it, if you are not using Windows and Visual Studio
+
+// #include "stdafx.h"
+
+
+// #include <iostream>
+
+#include <iostream>
+
+#include "opencv2/core/core.hpp"
+
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include "opencv2/highgui/highgui.hpp"
+
+
+// If you want to "simplify" code writing, you might want to use:
+
+// using namespace cv;
+
+// using namespace std;
+
+
+// AUXILIARY  FUNCTION
+
+void printImageFeatures(const cv::Mat &image)
+{
+	std::cout << std::endl;
+
+	std::cout << "Number of rows : " << image.size().height << std::endl;
+
+	std::cout << "Number of columns : " << image.size().width << std::endl;
+
+	std::cout << "Number of channels : " << image.channels() << std::endl;
+
+	std::cout << "Number of bytes per pixel : " << image.elemSize() << std::endl;
+
+	std::cout << std::endl;
+}
+
+
+// MAIN
+
+int main( int argc, char** argv )
+{
+    if( argc != 2 )
+    {
+		std::cout << "The name of the image file is missing !!" << std::endl;
+
+        return -1;
+    }
+
+	cv::Mat originalImage;
+
+	originalImage = cv::imread( argv[1], cv::IMREAD_UNCHANGED );
+
+	if( originalImage.empty() )
+	{
+		// NOT SUCCESSFUL : the data attribute is empty
+
+		std::cout << "Image file could not be open !!" << std::endl;
+
+	    return -1;
+	}
+
+	if( originalImage.channels() > 1 )
+	{
+		// Convert to a single-channel, intensity image
+		cv::cvtColor( originalImage, originalImage, cv::COLOR_BGR2GRAY, 1 );
+	}
+
+	// Create window
+    cv::namedWindow( "Imagem Original", cv::WINDOW_AUTOSIZE );
+	cv::imshow( "Imagem Original", originalImage );
+
+	// Print some image features
+	std::cout << "ORIGINAL IMAGE" << std::endl;
+    printImageFeatures( originalImage );
+
+	// Negative
+	cv::threshold(originalImage, originalImage, 120, 255, CV_THRESH_BINARY_INV);
+
+	cv::namedWindow( "Imagem Original after threshold", cv::WINDOW_AUTOSIZE );
+	cv::imshow( "Imagem Original after threshold", originalImage );
+
+	// Dilate
+	int dilation_size = 11;
+    cv::Mat result;
+	cv::Mat kernel = cv::getStructuringElement(2, cv::Size( dilation_size, dilation_size ));
+    
+	cv::erode(originalImage, result, kernel);
+    cv::namedWindow( "Erosion ", cv::WINDOW_AUTOSIZE );
+    cv::imshow( "Erosion ", result);
+
+    
+
+    // Waiting
+
+    cv::waitKey( 0 );
+
+	// Destroy the windows
+
+	cv::destroyAllWindows();
+
+	return 0;
+}
